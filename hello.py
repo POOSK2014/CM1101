3e416d9c909833dff3e458cb41c9dfc9f7ca47bb
POOSK2014 <paskin-2008@hotmail.co.uk>
2014-10-09 13:35:13 +0100

    yay, I commited my first file!

---
+++ hello.py
@@ -0,0 +1,5 @@
+Python 3.4.1 |Anaconda 2.0.1 (64-bit)| (default, Jun 11 2014, 17:27:11) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Hello")
+Hello
+>>> 
